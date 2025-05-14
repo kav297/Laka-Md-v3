8ac5565c3e7bbcf79395c56cf086c6d379813d72
kav297 <maxk14683@gmail.com>
2025-05-14 07:58:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Session id",
+    return text === fault ? true : fals
+SESSION_ID: process.env.SESSION_ID || "name:𝐋ᴀᴋᴀ-𝐌ᴅ=B1AgQC5Y#GbzHV9Su37HqoaMBtQDRL-H5q27x2ySeUCC8G7utM7c",
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
